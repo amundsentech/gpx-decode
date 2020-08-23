49cd5bf01247099cb4044b98db81bcf48195a6aa
Scott Close <scott@ethosgeo.com>
2020-08-23 22:45:05 +0000

    logs truncate for brevity

--- decode_test.go
+++ decode_test.go
@@ -11,17 +11,17 @@ import (
 
 const (
 	//testing datasets
-	points3D = "tests/testpoints.gpx"
-	lines    = "tests/testlines.gpx"
-	lines3D  = "tests/testlines with elevation.gpx"
+	points3D = "tests/points3D.gpx"
+	lines    = "tests/lines.gpx"
+	lines3D  = "tests/lines with elevation.gpx"
 )
 
 func TestGPX2Struct(t *testing.T) {
 
 	// build a map of the testing data and inputs
 	data := make(map[string]string)
-	//data[points3D] = points3D
-	//data[lines] = lines
+	data[points3D] = points3D
+	data[lines] = lines
 	data[lines3D] = lines3D
 
 	for item, fileloc := range data {
@@ -50,6 +50,6 @@ func TestGPX2Struct(t *testing.T) {
 
 		//fmt.Printf("Inbound reads like:\n%v\n", string(gpxbyte))
 		// what does it look like?
-		fmt.Printf("GPX struct as json reads:\n%v\n", string(gpxjson))
+		fmt.Printf("GPX struct as json reads:\n%v [...trunc...]\n", string(gpxjson)[0:1000])
 	}
 }
